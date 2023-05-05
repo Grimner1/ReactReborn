4fa290d5ca65de54eb4f4af9d9e39edbbe33de3d
Dmitry <61361063+Grimner1@users.noreply.github.com>
2023-05-05 11:42:57 +0300

    change 1

--- Lesson14/test4/src/App.jsx
+++ Lesson14/test4/src/App.jsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import ConnectionStatus from "./ConnectionStatus";
 
 const App = () => {
-  const [state, setState] = useState(true);
+  // const [state, setState] = useState(true);
 
-  const handleChange = () => {
-    setState(!state);
-  };
+  // const handleChange = () => {
+  //   setState(!state);
+  // };
 
   return (
     <div>
-      {state && <ConnectionStatus />}
-
-      <button onClick={handleChange}>{state ? "Close" : "Open"}</button>
+      {/* {state && <ConnectionStatus />} */}
+      <ConnectionStatus />
+      {/* <button onClick={handleChange}>{state ? "Close" : "Open"}</button> */}
     </div>
   );
 };
